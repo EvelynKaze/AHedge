c3785256fb75d8e6954fdea70a7ad0ea7f470913
VansRouges <evansagina57@gmail.com>
2023-05-04 09:56:21 +0100

    done with contact

--- src/components/contactHero.js
+++ src/components/contactHero.js
@@ -1,4 +1,6 @@
 import { GoPrimitiveDot } from 'react-icons/go'
+import { motion } from 'framer-motion';
+
 
 const features = [
   {
@@ -18,7 +20,7 @@ const features = [
     icon: GoPrimitiveDot,
   },
   {
-    name: 'Email: investmoon@domain.com',
+    name: 'Email: support@wallethedge.org',
     icon: GoPrimitiveDot,
   },
   {
@@ -32,9 +34,33 @@ export default function Example() {
     <div className="overflow-hidden bg-white py-24 sm:py-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div>
-            
-          </div>
+        <motion.div
+              initial={{
+                y:-100,
+                opacity:0,
+            }}
+            transition={{ duration: 1.0 }}
+            whileInView={{ opacity: 1, y:-20 }}
+            viewport={{ once:true }}
+            className="shadow-xl lg:w-5/6 md:w-1/2 bg-gray-100 rounded p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+            <div className="relative mb-4">
+              <label className="leading-7 text-base text-gray-600">First Name *</label>
+              <input type="text" name="full-name" className="w-full text-gray-700 py-1 px-3 bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none leading-8" />
+            </div>
+            <div className="relative mb-4">
+              <label className="leading-7 text-base text-gray-600">Email *</label>
+              <input type="email" name="email" className="w-full text-gray-700 py-1 px-3 bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none leading-8" />
+            </div>
+            <div className="relative mb-4">
+              <label className="leading-7 text-base text-gray-600">Subject *</label>
+              <input type="text" name="subject" className="w-full text-gray-700 py-1 px-3 bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none leading-8" />
+            </div>
+            <div className="relative mb-4">
+              <label className="leading-7 text-base text-gray-600">Message *</label>
+              <textarea type="text" name="message" className="w-full text-gray-700 py-1 px-3 bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none leading-8 "></textarea>
+            </div>
+            <button className="text-white align-left text-center w-28 text-xl bg-blue-500 border-0 py-2 px-4 focus:outline-none hover:bg-gray-100 hover:border-2 hover:border-blue-500 hover:text-blue-500 rounded-3xl">Submit</button>
+          </motion.div>
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-xl">
               <h2 className="text-base font-semibold leading-7 text-indigo-600 capitalize">Get In Touch</h2>
