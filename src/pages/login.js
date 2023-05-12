9adeda059c26dfc9255f7aebd1ad9f403c63a1a8
VansRouges <evansagina57@gmail.com>
2023-05-12 05:10:16 +0100

    register and login UI done

---
+++ src/pages/login.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { motion } from 'framer-motion';
+import Link from "next/link"
+
+
+
+
+
+export default function Example() {
+    return (
+      <>
+      <Navbar />
+        {/*
+          This example requires updating your template:
+  
+          ```
+          <html class="h-full bg-white">
+          <body class="h-full">
+          ```
+        */}
+        <motion.div 
+            initial={{
+                y:-120,
+                opacity:0,
+            }}
+            transition={{ duration: 1.0 }}
+            whileInView={{ opacity: 1, y:-70 }}
+            className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+            >
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+            className="mx-auto h-20 w-20"
+            src="https://bpvsklhytoplnehaskcs.supabase.co/storage/v1/object/sign/avatars/wallethedge-logo-removebg.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzL3dhbGxldGhlZGdlLWxvZ28tcmVtb3ZlYmcucG5nIiwiaWF0IjoxNjgzODYyMTY0LCJleHAiOjE3MTUzOTgxNjR9.CGRl6UcT7CpOgClUWeMJd6BFah44znWUCCgzBnI-ebc&t=2023-05-12T03%3A29%3A25.605Z"
+            alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+            </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+            <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+                </label>
+                <div className="mt-2">
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                </div>
+            </div>
+
+            <div>
+                <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                </label>
+                <div className="text-sm block">
+                    <a href="#" className=" float-right font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                    </a>
+                </div>
+                </div>
+                <div className="mt-2">
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                </div>
+            </div>
+
+            <div>
+                <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                Sign in
+                </button>
+            </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+            Don&#39;t have an account?{' '}
+            <Link href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                Sign up
+            </Link>
+            </p>
+        </div>
+        </motion.div>
+
+        
+      </>
+    )
+  }
+
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { motion } from 'framer-motion';
+import Link from "next/link"
 
 
 
@@ -19,18 +20,97 @@ const register = () => {
                         opacity:0,
                     }}
                     transition={{ duration: 1.0 }}
-                    whileInView={{ opacity: 1, y:-20 }}
+                    whileInView={{ opacity: 1, y:-40 }}
                     viewport={{ once:true }}
-                    className="shadow-xl lg:w-4/6 md:w-1/2 bg-gray-100 rounded p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-                    <div className="relative mb-4">
-                        <label className="leading-7 text-base text-gray-600">Email *</label>
-                        <input type="email" name="email" className="w-full text-gray-700 py-1 px-3 bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none leading-8" />
-                    </div>
-                    <div className="relative mb-4">
-                        <label className="leading-7 text-base text-gray-600">Password *</label>
-                        <input type="password" name="password" className="w-full text-gray-700 py-1 px-3 bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none leading-8" />
-                    </div>
-                    <button className="text-white align-left text-center w-28 text-xl bg-blue-500 border-0 py-2 px-4 focus:outline-none hover:bg-gray-100 hover:border-2 hover:border-blue-500 hover:text-blue-500 rounded-3xl">Submit</button>
+                    className="shadow-xl rounded w-full"
+                    >
+                    {/* <h2 className="text-center font-bold text-xl md:text-2xl lg:text-3xl font-heading">Register</h2> */}
+                    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <img
+            className="mx-auto h-10 w-auto"
+            src="https://bpvsklhytoplnehaskcs.supabase.co/storage/v1/object/sign/avatars/wallethedge-logo-mobile-blue.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzL3dhbGxldGhlZGdlLWxvZ28tbW9iaWxlLWJsdWUud2VicCIsImlhdCI6MTY4MTM5NjI0MywiZXhwIjoxNzEyOTMyMjQzfQ.Bh4ylJ6QuHIVzw72bggZkdLKo8M_v5T0m53cwM9SFew&t=2023-04-13T14%3A30%3A42.585Z"
+            alt="Your Company"
+          /> */}
+          <h2 className="text-left mt-10 text-2xl font-semibold leading-9 tracking-tight text-gray-900">
+            Create an Account
+          </h2>
+        </div>
+
+        <div className="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+          <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Full Name
+              </label>
+              <div>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                {/* <div className="text-xs">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Already have an account?{' '}
+            <Link href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Log In
+            </Link>
+          </p>
+        </div>
+      </div>
                 </motion.div>
                 
                 <div className="lg:pr-8 lg:pt-4">
