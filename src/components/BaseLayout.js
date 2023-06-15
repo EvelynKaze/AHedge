e356551dddd88f1ae06bbd8635250908c88abcd7
VansRouges <evansagina57@gmail.com>
2023-06-15 15:01:50 +0100

    changed api key

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -4,7 +4,7 @@ const BaseLayout = ({ children }) => {
   return (
     <div className="flex relative max-h-max gap-8">
       <Sidebar />
-      <main className="grow-0 lg:grow">{children}</main>;
+      <main className="grow-0 lg:grow">{children}</main>
     </div>
   );
 };
