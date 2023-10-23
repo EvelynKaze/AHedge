d0e2cd7a27279664c31b3194bd20c260e4479ea4
VansRouges <evansagina57@gmail.com>
2023-10-24 00:23:24 +0100

    restored head link

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" /> */}
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
       </Head>
       <body>
         <Main />
