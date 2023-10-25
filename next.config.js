965b17d10c2db8fb6d9e75f55517b079d4104b8b
VansRouges <evansagina57@gmail.com>
2023-10-25 16:55:44 +0100

    achieved i18n

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const { i18n } = require('./next-i18next.config')
 
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
   swcMinify: true,
-  
 }
 
 
@@ -13,10 +13,6 @@ const nextConfig = {
 module.exports = {
   images: {
     domains: ['https://bpvsklhytoplnehaskcs.supabase.co/storage/v1/object/sign/avatars'],
-  },
-  i18n: {
-    locales: ['en', 'bn'],
-    defaultLocale: 'en',
   },
 };
 
