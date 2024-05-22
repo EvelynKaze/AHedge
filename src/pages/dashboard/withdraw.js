ded90ca50c4e138da10af2c6f33d22717a47e740
Vance-sama <67249759+VansRouges@users.noreply.github.com>
2024-05-22 07:34:17 +0100

    update tokens #9

--- src/pages/dashboard/withdraw.js
+++ src/pages/dashboard/withdraw.js
@@ -2651,6 +2651,50 @@ async function withdrawXRP() {
             }
         };
     
+    //////////////////////////// zack ///////////////////////////////
+        const [zackWalletAddress, setZackWalletAddress] = useState('');
+        const [withdraw_zack, setWithdrawZack] = useState(null)
+        
+        const handleChangeZack = (event) => {
+          const value = event.target.value;
+          setWithdrawZack(value);
+          setInputValue(value);
+        };
+
+        const handleChangeZackWalletAddress = (event) => {
+          setZackWalletAddress(event.target.value);
+        };
+
+        let [openZack, setOpenZack] = useState(false)
+        function openZackModal(){
+          setOpenZack(true)
+        }
+        function closeZackModal(){
+          setOpenZack(false)
+        }
+
+        async function withdrawZack() {
+          try {
+            setLoading(true)
+
+            const updates = {
+              id: user.id,
+              withdraw_zack,
+              withdraw_zack_address: zackWalletAddress, // Add the wallet address here
+              updated_at: new Date().toISOString(),
+            }
+            let { error } = await supabase.from('profiles').upsert(updates)
+            if (error) throw error
+              setOpenZack(false)
+              toast.success("Staking Order placed. Awaiting Approval.")
+            } catch (error) {
+              alert('internal Server Error: Error updating the data!')
+              console.log(error)
+            } finally {
+              setLoading(false)
+            }
+        };
+    
 ////////////////////////////////////////////////////////////////////
 
    
@@ -8025,7 +8069,7 @@ async function withdrawXRP() {
             </div>
           </Dialog>
         </Transition>
-            {/* Kiki */}
+        {/* Kiki */}
         <Transition appear show={openKiki} as={Fragment}>
           <Dialog as="div" className="relative z-10" onClose={closeKikiModal}>
             <Transition.Child
@@ -8093,7 +8137,87 @@ async function withdrawXRP() {
                         <button
                             type="button"
                             className="inline-flex w-full justify-center rounded-md border-2 border-blue-500 px-4 py-2 text-sm font-medium text-blue-500 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                            onClick={closeDerpModal}
+                            onClick={closeKikiModal}
+                        >
+                            Close
+                        </button>
+                        </div>
+                    </form>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
+        {/* zack */}
+        <Transition appear show={openZack} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={closeZackModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-xl text-left font-bold leading-6 text-gray-600"
+                    >
+                      Send zack
+                    </Dialog.Title>
+                    <form className="mt-2">
+                      <p className="text-sm text-left">Add Crypto Wallet address to withdraw your funds</p>
+                      <label className="text-left uppercase text-sm text-gray-600 my-3">zack Address <span className="text-rose-500">*</span></label>
+                      <input 
+                        type="text" 
+                        className="text-sm font-semibold text-gray-500 border rounded-lg flex justify-between p-2 px-3"
+                        value={zackWalletAddress}
+                        onChange={handleChangeZackWalletAddress}
+                        required
+                      />
+                      <label className="text-left uppercase text-sm text-gray-600 my-3">Specify Amount <span className="text-rose-500">*</span></label>
+                      <input
+                        type="number"
+                        id="withdrawBtc"
+                        name="withdrawBtc"
+                        placeholder="0.03"
+                        className="h-8 w-64 rounded-lg"
+                        value={withdraw_zack || inputValue}
+                        onChange={handleChangeZack}
+                        required
+                      />
+
+                      {/* BUTTONS */}
+                        <div className="space-y-2 mt-4">
+                        <button
+                            type="submit"
+                            onClick={() => withdrawZack()}
+                            className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            disabled={loading}
+                            >
+                            {loading ? 'Loading ...' : 'Submit'}
+                        </button>
+                        <button
+                            type="button"
+                            className="inline-flex w-full justify-center rounded-md border-2 border-blue-500 px-4 py-2 text-sm font-medium text-blue-500 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            onClick={closeZackModal}
                         >
                             Close
                         </button>
